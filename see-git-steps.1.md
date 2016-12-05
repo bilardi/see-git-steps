69bf45c0a893aecc974b50168bf84aa6a627d6f9
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2016-12-05 14:25:40 +0100

    step 4 - add help function: make test works properly, make man works properly

---
+++ see-git-steps.1.md
@@ -0,0 +1,57 @@
+see-git-steps(1) -- See git commits step by step
+=================================
+
+## SYNOPSIS
+
+`see-git-steps` [-hVtvis] [-c <commit>]
+
+## OPTIONS
+
+  `-h, --help`              display this message
+
+  `-V, --version`           output version
+
+  `-t, --test`              display the result of the basic test
+
+  `-v, --verbose`           enable verbose output
+
+  `-c, --commit`            number of <commit> where to start
+
+  `-i, --interaction`       display one commit at a time and expect enter or [yY] to display the next commit
+
+  `-s, --skip-question`     no display the question between each commit
+
+## USAGE
+
+  $ see-git-steps -t
+    display OK if you have installed on your device git program
+    display KO if you don't have installed on your device git program
+
+  $ see-git-steps -c 9680dc4
+    display only description of the commits from old to new until the <commit>,
+    then if the description starts with `step [0-9]*`, display also the file of the commit
+
+  $ see-git-steps -c 9680dc4 -v
+    display only description of the commits from old to new until the <commit>,
+    then display the file of the commit and if the description starts with `step [0-9]*`, display also the diff
+
+  $ see-git-steps -v -i -s
+    display one commit at a time and expect enter or [yY] to display the next commit
+    display the file of the commit and if the description starts with `step [0-9]*`, display also the diff
+
+## AUTHOR
+
+  Alessandra Bilardi <alessandra.bilardi@gmail.com>
+
+## REPORTING BUGS
+
+  https://github.com/bilardi/see-git-steps/issues
+
+## SEE ALSO
+
+  `git`(1), `git-log`(1)
+
+## LICENSE
+
+  MIT (C) Copyright Alessandra Bilardi 2016
+
--- see-git-steps.sh
+++ see-git-steps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # initialize
-VERSION="0.0.1"
+VERSION="0.0.2"
 
 # functions
 
@@ -13,10 +13,78 @@ function version {
     echo "See git steps: $VERSION"
 }
 
+# The script help
+#
+# Returns:
+#   string: script manual
+function help {
+    cat <<EOF
+see-git-steps(1) -- See git commits step by step
+=================================
+
+## SYNOPSIS
+
+\`see-git-steps\` [-hVtvis] [-c <commit>]
+
+## OPTIONS
+
+  \`-h, --help\`              display this message
+
+  \`-V, --version\`           output version
+
+  \`-t, --test\`              display the result of the basic test
+
+  \`-v, --verbose\`           enable verbose output
+
+  \`-c, --commit\`            number of <commit> where to start
+
+  \`-i, --interaction\`       display one commit at a time and expect enter or [yY] to display the next commit
+
+  \`-s, --skip-question\`     no display the question between each commit
+
+## USAGE
+
+  $ see-git-steps -t
+    display OK if you have installed on your device git program
+    display KO if you don't have installed on your device git program
+
+  $ see-git-steps -c 9680dc4
+    display only description of the commits from old to new until the <commit>,
+    then if the description starts with \`step [0-9]*\`, display also the file of the commit
+
+  $ see-git-steps -c 9680dc4 -v
+    display only description of the commits from old to new until the <commit>,
+    then display the file of the commit and if the description starts with \`step [0-9]*\`, display also the diff
+
+  $ see-git-steps -v -i -s
+    display one commit at a time and expect enter or [yY] to display the next commit
+    display the file of the commit and if the description starts with \`step [0-9]*\`, display also the diff
+
+## AUTHOR
+
+  Alessandra Bilardi <alessandra.bilardi@gmail.com>
+
+## REPORTING BUGS
+
+  https://github.com/bilardi/see-git-steps/issues
+
+## SEE ALSO
+
+  \`git\`(1), \`git-log\`(1)
+
+## LICENSE
+
+  MIT (C) Copyright Alessandra Bilardi 2016
+
+EOF
+    exit 1
+}
+
 # main
 ## get options of the script
 while true; do
     case "$1" in
+	-h | --help ) help ;;
 	-V | --version ) version; exit 1; ;;
 	-- ) shift; break ;;
 	* ) break ;;
